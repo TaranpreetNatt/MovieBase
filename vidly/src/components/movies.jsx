434ade6e0c5ac256179d488241250305c42888b5
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-09-10 19:16:07 -0700

    added an npm package propTypes for typechecking and refactored movies component

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Like from '../common/like';
+import Pagination from '../common/pagination';
+import { paginate } from '../utils/paginate';
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1,
   };
 
   handleDelete = (movie) => {
@@ -20,10 +24,17 @@ class Movies extends Component {
     this.setState({ movies })
   }
 
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  }
+
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
 
     if(count === 0) return <p style={{ paddingTop: 20 }}>There are no movies in the database</p>
+
+    const movies = paginate(allMovies, currentPage, pageSize);
 
     return (
       <React.Fragment>
@@ -39,7 +50,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {movies.map(movie => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
@@ -47,7 +58,7 @@ class Movies extends Component {
                 <td>{movie.dailyRentalRate}</td>
                 <td>
                   <Like
-                    like={ movie.like }
+                    like={movie.like}
                     onLike={ () => this.handleLike(movie) }
                   />
                 </td>
@@ -60,6 +71,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
