e4b5c2feb91b08fac14f8f2ac682b39b15bcd33d
TaranpreetNatt <taranpreetnatt12@hotmail.com>
2019-09-14 21:24:09 -0700

    removed table component from moviesTable to a reusable table components

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Like from '../common/like';
-import TableHeader from "../common/tableHeader";
+import Table from '../common/table';
 
 class MoviesTable extends Component {
 
@@ -9,47 +9,39 @@ class MoviesTable extends Component {
     { path: 'genre.name', label: 'Genre'},
     { path: 'numberInStock', label: 'Stock'},
     { path: 'dailyRentalRate', label: 'Rate'},
-    { key: 'like'},
-    { key: 'delete'},
+    { 
+      key: 'like', 
+      content: movie => (
+        <Like
+          like={movie.like} 
+          onLike={() => this.props.onLike(movie)} 
+        />
+      )
+    },
+    {
+      key: 'delete',
+      content: movie => (
+        <button 
+          onClick={() => this.props.onDelete(movie)} 
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      )
+    },
   ];
-
+  
   render() { 
-  const { movies, onDelete, onLike } = this.props;
-
-
-  return ( 
-    <table className="table">
-      <TableHeader
-        columns={this.columns}
-        sortColumn={this.props.sortColumn}
-        onSort={this.props.onSort}
-      />
-      <TableBody
+    const { movies, sortColumn, onSort } = this.props;
+    return (
+      <Table
         data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+        columns={this.columns}
       />
-      <tbody>
-        {movies.map(movie => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <Like
-                like={movie.like}
-                onLike={ () => onLike(movie) }
-            />
-            </td>
-            <td>
-              <button onClick={() => onDelete(movie)} className="btn btn-danger btn-sm">
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    )
+
   }
 }
   
